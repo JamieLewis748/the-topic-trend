41740bdf7275aea96b78da248ed0ff80e832637b
Jamie Lewis <Jamie@Jamies-MacBook-Air.local>
2023-10-19 09:27:41 +0700

    adds type 'Categories'

--- app/NavLinks.tsx
+++ app/NavLinks.tsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
+
 function NavLinks() {
     return (
-        <></>
+        <Link href={`/news/${category}`} className={`navLink ${isActive && 'underline decoration-purple-400 underline-offset-4 font-bold text-lg'}`}>
+            {category}
+        </Link>
     )
 }
 
