49d6b77d15ee723aca55aed52436194f3ae6aeb8
JamieLewis748 <j.h.lewis89@gmail.com>
2023-10-19 17:44:14 +0700

    adds extra categories, completes NavLinks

--- constants.ts
+++ constants.ts
@@ -2,4 +2,11 @@ export const categories: Category[] = [
     "coding",
     "football",
     "cooking",
+    "general",
+    "business",
+    "entertainment",
+    "health",
+    "science",
+    "sports",
+    "technology",
 ];
