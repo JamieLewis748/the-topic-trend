d5041dbd1750e0284a66b9658705b569414e7cf6
JamieLewis748 <j.h.lewis89@gmail.com>
2023-10-31 16:28:30 +0700

    remove console log

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -5,8 +5,6 @@ try {
     const response = await axios.get("https://nc-news-app-wdjy.onrender.com/api/articles");
 
     const data = response.data;
-    console.log("🚀 ~ data:", data);
-
     return data;
 } catch (error) {
     console.error("An error occurred while fetching news:", error);
