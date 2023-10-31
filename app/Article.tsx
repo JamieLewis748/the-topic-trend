42d88230f108d19b9fd6e8eeb8d6ccdd7f1cbe78
JamieLewis748 <j.h.lewis89@gmail.com>
2023-10-31 16:24:18 +0700

    adds article properties and styling

---
+++ app/Article.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+type Props = {
+    article: Article;
+}
+
+function Article({article}: Props) {
+return (
+    <article className="bg-slate-100 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
+        {article.article_img_url && (
+            <img
+                src={article.article_img_url}
+                alt={article.title}
+                className="h-56 w-full object-cover rounded-t-lg shadow-md"
+            />
+        )}
+        <div className="flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col p-5">
+                <h2 className="font-bold font-serif">{article.title}</h2>
+                <section className="mt-2 flex-1">
+                    <p className="text-xs line-clamp-6">Lorem Ipsum</p>
+                </section>
+                <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
+                    <p>{article.author}</p>
+                    <p>{new Date(article.created_at).toLocaleDateString()} {new Date(article.created_at).toLocaleTimeString()}</p>
+
+                </footer>
+
+            </div>
+        </div>
+    </article>
+)
+}
+
+export default Article
