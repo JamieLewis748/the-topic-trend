5948201ca254d1ad901154a9a9147be895b60d8e
Jamie Lewis <Jamie@Jamies-MacBook-Air.local>
2023-10-31 17:41:46 +0700

    adds dark mode styling to background

--- app/Article.tsx
+++ app/Article.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function Article({article}: Props) {
 return (
-    <article className="bg-slate-100 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
         {article.article_img_url && (
             <img
                 src={article.article_img_url}
