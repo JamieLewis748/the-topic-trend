132ea90bb09b52d14a6b66e29983af00a551f58a
Jamie Lewis <Jamie@Jamies-MacBook-Air.local>
2023-11-01 17:29:43 +0700

    adds topic params to fetchNews function

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-const fetchNews = async () => {
-try {
-    const response = await axios.get("https://nc-news-app-wdjy.onrender.com/api/articles");
+const fetchNews = async (
+    topic?: string
+    ) => {
+    try {
+        const apiUrl = "https://nc-news-app-wdjy.onrender.com/api/articles";
+        const params = { topic }; 
 
-    const data = response.data;
-    return data;
+        const response = await axios.get(apiUrl, { params });
+        const data = response.data;
+        
+        return data;
 } catch (error) {
     console.error("An error occurred while fetching news:", error);
 }
