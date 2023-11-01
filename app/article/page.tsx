43272344aa1a061b8d20f623c51256d2c446a6d5
JamieLewis748 <j.h.lewis89@gmail.com>
2023-11-01 15:18:13 +0700

    adds fetchArticle function and changes property names to match new data

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,38 +1,46 @@
 
+import fetchArticle from "@/lib/fetchArticle";
 import { notFound } from "next/navigation"
 
 type Props = {
     searchParams?: Article
 }
 
-function ArticlePage({ searchParams }: Props) {
+async function ArticlePage({ searchParams }: Props) {
     if ((searchParams && Object.entries(searchParams).length === 0) ||
         !searchParams) {
         return notFound()
     }
 
     const article: Article = searchParams
+
+    const { article_id } = article
+    const articleById = await fetchArticle(article_id)
+
     
     return (
         <article>
-            <section>
-                {article.article_img_url && (
+            <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
+                {articleById.article_img_url && (
                     <img
                         className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
-                        src={article.article_img_url}
-                    alt={article.title} />
+                        src={articleById.article_img_url}
+                    alt={articleById.title} />
                 )}
                 <div className="px-10">
                     <h1 className="headerTitle px-0 no-underline pb-2">
-                        {article.title}
+                        {articleById.title}
                     </h1>
 
-                    <div className="flex divide-x-2 space-x-4">
-                        <h2 className="font-bold">By: {article.author}</h2>
-                        <p className="pl-4">{new Date(article.created_at).toLocaleDateString()} {new Date(article.created_at).toLocaleTimeString()}</p>
+                    <div className="flex divide-x-2 space-x-4 ">
+                        <h2 className="font-bold">By: {articleById.author}</h2>
+                        <p className="pl-4">{new Date(articleById.created_at).toLocaleDateString()} {new Date(articleById.created_at).toLocaleTimeString()}</p>
                     </div>
+                    <p className="pt-4 pb-20">{articleById.body}</p>
                 </div>
             </section>
+            {/*add comments section*/}
+            {/*add votes section*/}
         </article>
   )
 }
