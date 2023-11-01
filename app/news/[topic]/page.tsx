587a3ff91d3fe04fd3adc15063f383662f0fc780
JamieLewis748 <j.h.lewis89@gmail.com>
2023-11-01 17:29:05 +0700

    adds Topic component

---
+++ app/news/[topic]/page.tsx
@@ -0,0 +1,24 @@
+import Newslist from "@/app/Newslist";
+import fetchNews from "@/lib/fetchNews";
+import { notFound } from "next/navigation";
+
+type Props = {
+    params: {topic: Topic}
+}
+
+async function Topic({params: {topic}}: Props) {
+const news: NewsResponse = await fetchNews(topic)
+
+if (news.articles.length === 0) {
+    return notFound();
+  }
+
+return (
+    <div>
+        <h1 className="headerTitle">{topic}</h1>
+        <Newslist news={news} />
+    </div>
+)
+}
+
+export default Topic
