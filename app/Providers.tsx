9dc67d2edf611f91b492101c03e838fd886e1000
JamieLewis748 <j.h.lewis89@gmail.com>
2023-11-09 22:10:04 +0700

    adds UserProvider

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,9 +1,12 @@
 "use client"
+import { UserProvider } from "@/context/userContext";
 import { ThemeProvider } from "next-themes";
 
 function Providers({children}: {children: React.ReactNode;}) {
   return (
+<UserProvider>
     <ThemeProvider enableSystem={true} attribute="class">{children}</ThemeProvider>
+</UserProvider>
   )
 }
 
