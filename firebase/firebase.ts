b1c4e8a621961175cfe5315da6567d126e259ecf
Jamie Lewis <Jamie@Jamies-MacBook-Air.local>
2023-11-09 22:13:48 +0700

    deletes old file

--- firebase/firebase.ts
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import firebaseConfig from "@/firebaseConfig";
-
-const fb = firebase.initializeApp(firebaseConfig);
-
-export default fb;
