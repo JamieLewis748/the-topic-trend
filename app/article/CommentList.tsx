9a2dc458d1c8990327ff75d2fd2628340c9aa88e
Jamie Lewis <j.h.lewis89@gmail.com>
2023-11-10 17:21:00 +0700

    adds comments card

---
+++ app/article/CommentList.tsx
@@ -0,0 +1,9 @@
+import fetchCommentsByArticleId from "@/lib/fetchCommentsByArticleId"
+
+function CommentList() {
+  return (
+    <div>This is where the comments will go</div>
+  )
+}
+
+export default CommentList
