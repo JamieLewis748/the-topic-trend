78c1633ef437f1ff192880de1809ced13f6ccf64
Jamie Lewis <j.h.lewis89@gmail.com>
2023-11-13 17:07:52 +0700

    adds CommentList and CommentAdder to page

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,31 +1,35 @@
 
 import fetchArticle from "@/lib/fetchArticle";
-import { notFound } from "next/navigation"
+import { notFound } from "next/navigation";
+import CommentList from "./CommentList";
+import CommentAdder from "./CommentAdder";
+import fetchCommentsByArticleId from "@/lib/fetchCommentsByArticleId";
 
 type Props = {
-    searchParams?: Article
-}
+    searchParams?: Article;
+};
 
 async function ArticlePage({ searchParams }: Props) {
     if ((searchParams && Object.entries(searchParams).length === 0) ||
         !searchParams) {
-        return notFound()
+        return notFound();
     }
 
-    const article: Article = searchParams
+    const article: Article = searchParams;
 
-    const { article_id } = article
-    const articleById = await fetchArticle(article_id)
+    const { article_id } = article;
+    const articleById = await fetchArticle(article_id);
+    const comments: CommentList = await fetchCommentsByArticleId(article_id);
 
-    
+
     return (
         <article>
-            <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
+            <section className="flex flex-col lg:flex-row pb-5 px-0 lg:px-10 border-slate-400	">
                 {articleById.article_img_url && (
                     <img
                         className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
                         src={articleById.article_img_url}
-                    alt={articleById.title} />
+                        alt={articleById.title} />
                 )}
                 <div className="px-10">
                     <h1 className="headerTitle px-0 no-underline pb-2">
@@ -39,10 +43,11 @@ async function ArticlePage({ searchParams }: Props) {
                     <p className="pt-4 pb-20">{articleById.body}</p>
                 </div>
             </section>
-            {/*add comments section*/}
+            <CommentAdder />
+            <CommentList comments={comments} />
             {/*add votes section*/}
         </article>
-  )
+    );
 }
 
-export default ArticlePage
+export default ArticlePage;
