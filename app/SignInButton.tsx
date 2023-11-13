07176fc115f2fc951e871f6e1c3168484618ff2f
Jamie Lewis <j.h.lewis89@gmail.com>
2023-11-13 17:10:12 +0700

    adds sign out

--- app/SignInButton.tsx
+++ app/SignInButton.tsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 
 import { useUser } from '@/context/userContext';
 import { initFirebase } from '@/firebase/firebase.config';
 import fetchUser from '@/lib/fetchUser';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider } from 'firebase/auth';
 import { redirect } from 'next/navigation';
 import RegistrationModal from './RegistrationModal';
 import { useState } from 'react';
@@ -22,63 +22,82 @@ function SignInButton() {
 
     const closeRegistrationModal = () => {
         setRegistrationModalOpen(false);
+        if (user && !user.username) {
+            signOut(auth)
+                .then(() => {
+                    setUser(null);
+                })
+                .catch((error) => {
+                    console.error('Error during sign-out:', error);
+                });
+        }
+    };
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser(null);
+            })
+            .catch((error) => {
+                console.error('Error during sign-out:', error);
+            });
     };
 
 
     const signIn = async () => {
-    try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
+        try {
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
 
-        if (user.email) {
-        fetchUser(user.email)
-            .then((existingUser) => {
-            if (existingUser) {
-                setUser(existingUser);
-            } else {
-                const newUser = {
-                    username: '',
-                    name: user.displayName || '',
-                    email: user.email || '',
-                    avatar_url: user.photoURL || '',
-                    };
-                setUser(newUser);
-                openRegistrationModal()
-                }
-            })
-            .catch((error) => {
-            console.error('Error fetching user data:', error);
-            });
+            if (user.email) {
+                fetchUser(user.email)
+                    .then((existingUser) => {
+                        if (existingUser) {
+                            setUser(existingUser);
+                        } else {
+                            const newUser = {
+                                username: '',
+                                name: user.displayName || '',
+                                email: user.email || '',
+                                avatar_url: user.photoURL || '',
+                            };
+                            setUser(newUser);
+                            openRegistrationModal();
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching user data:', error);
+                    });
+            }
+        } catch (error) {
+            console.error('Error during Google Sign-In:', error);
         }
-    } catch (error) {
-        console.error('Error during Google Sign-In:', error);
-    }
-    }
+    };
 
     return (
         <div>
-        {user && user.username ? ( 
-                <div className="md:flex hidden bg-slate-900 text-white px-4 lg:px-4 py-1 lg:py-2 rounded-full" >
-                    <span className="ml-10 mr-5 mt-1">{user.name}</span> 
+            {user && user.username ? (
+                <button onClick={handleSignOut} className="md:flex w-25 hidden bg-slate-900 text-white px-4 lg:px-4 py-2 dark:bg-slate-600 lg:py-2 rounded-full" >
+                    <span className="ml-10 mr-5 mt-1">Sign Out</span>
                     <img
-                        src={user.avatar_url} 
+                        src={user.avatar_url}
                         alt="User Avatar"
                         style={{ width: '30px', height: '30px', borderRadius: '50%' }}
                     />
-                </div>
+                </button>
             ) : (
                 <button
                     onClick={signIn}
-                    className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full"
+                    className="hidden md:flex w-25 bg-slate-900 text-white px-4 dark:bg-slate-600 lg:px-8 py-2 lg:py-4 rounded-full"
                 >
-                    Subscribe Now
+                    Sign In
                 </button>
             )}
-        {isRegistrationModalOpen && (
+            {isRegistrationModalOpen && (
                 <RegistrationModal onClose={closeRegistrationModal} />
-    )}
-    </div>
+            )}
+        </div>
     );
-    }
+}
 
 export default SignInButton;
