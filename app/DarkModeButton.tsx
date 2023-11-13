f049cfb118c6ffb3a004d35c7d5e2d80fa2d852d
Jamie Lewis <j.h.lewis89@gmail.com>
2023-11-13 17:13:52 +0700

    formatted

--- app/DarkModeButton.tsx
+++ app/DarkModeButton.tsx
@@ -1,15 +1,15 @@
-'use client'
+'use client';
 import { useTheme } from "next-themes";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
 
 function DarkModeButton() {
-    const [mounted, setMounted] = useState(false)
-    const {systemTheme, theme, setTheme} = useTheme()
+    const [mounted, setMounted] = useState(false);
+    const { systemTheme, theme, setTheme } = useTheme();
 
     useEffect(() => {
         setMounted(true);
-}, [])
+    }, []);
 
     if (!mounted) {
         return null;
@@ -24,13 +24,13 @@ function DarkModeButton() {
             <SunIcon
                 className="h-8 w-8 cursor-pointer text-yellow-500"
                 onClick={() => setTheme("light")} />
-        ): (
+        ) : (
             <MoonIcon
                 className="h-8 w-8 cursor-pointer text-gray-900"
                 onClick={() => setTheme("dark")} />
-            )}
-            </div>
-    )
+        )}
+        </div>
+    );
 }
 
-export default DarkModeButton
+export default DarkModeButton;
