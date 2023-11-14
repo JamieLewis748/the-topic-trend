05b7943450bee641205be71c796671fe9d9307fc
Jamie Lewis <j.h.lewis89@gmail.com>
2023-11-14 08:11:58 +0700

    update Props to fix issue with map

--- app/article/CommentList.tsx
+++ app/article/CommentList.tsx
@@ -1,20 +1,18 @@
-import CommentCard from "./CommentCard";
+import React from 'react';
+import CommentCard from './CommentCard';
 
-
-type Props = {
-  comments: CommentList;
+type CommentListProps = {
+  comments: MyComment[];
 };
 
-function CommentList({ comments }: Props) {
-
-
+const CommentList: React.FC<CommentListProps> = ({ comments }) => {
   return (
-    <main className="flex flex-col p-5 w-6xl">{
-      comments.map((comment: MyComment) => (
+    <main className="flex flex-col p-5 w-6xl">
+      {comments.map((comment: MyComment) => (
         <CommentCard key={comment.comment_id} comment={comment} />
-
       ))}
     </main>
   );
-}
+};
+
 export default CommentList;
